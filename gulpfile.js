dca6d8d07fe4eae3ace645e0de58cd34bcb1fee8
Mads Thines <mads.thines@adapt.dk>
2018-04-11 16:44:24 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,29 @@
 "use strict";
 
 // Modules
-const gulp          = require('gulp'),
-      postcss       = require('gulp-postcss'),
-      sass          = require('gulp-sass'),
-      plumber       = require('gulp-plumber'),
-      sourcemaps    = require('gulp-sourcemaps'),
-      inlinesvg     = require('postcss-inline-svg'),
-      autoprefixer  = require('autoprefixer'),
-      cssnano       = require('cssnano'),
-      gulpStylelint = require('gulp-stylelint'),
-      tildeImporter = require('node-sass-tilde-importer'),
-      browserSync   = require('browser-sync').create();
+const gulp = require('gulp'),
+  postcss = require('gulp-postcss'),
+  util = require('gulp-util'),
+  sass = require('gulp-sass'),
+  plumber = require('gulp-plumber'),
+  sourcemaps = require('gulp-sourcemaps'),
+  inlinesvg = require('postcss-inline-svg'),
+  autoprefixer = require('autoprefixer'),
+  cssnano = require('cssnano'),
+  gulpStylelint = require('gulp-stylelint'),
+  tildeImporter = require('node-sass-tilde-importer'),
+  changed = require('gulp-changed'),
+  browserSync = require('browser-sync').create();
 
 // Paths
 const pathSrc = './src/**/*.scss';
 const pathDist = './dist';
+const production = !!util.env.production;
 
 // SCSS
 gulp.task('scss', function() {
   // Preprocessor
-  const processors = [
+  let processors = [
     autoprefixer({ // Autoprefixer
       browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1'],
       remove: true
@@ -28,18 +31,24 @@ gulp.task('scss', function() {
     inlinesvg({ // Inline SVG's
       path: './public/svg'
     }),
-    cssnano({ // Minify
-      zindex: false,
-      reduceIdents: {
-        keyframes: false
-      },
-      discardUnused: {
-        keyframes: false
-      }
-    })
   ];
 
+  if (production) {
+    processors.push(
+      cssnano({ // Minify
+        zindex: false,
+        reduceIdents: {
+          keyframes: false
+        },
+        discardUnused: {
+          keyframes: false
+        }
+      })
+    )
+  }
+
   return gulp.src(pathSrc)
+      
              // Plumber prevents Gulp from breaking because of errors
              .pipe(plumber({
                errorHandler: function(error) {
@@ -48,15 +57,19 @@ gulp.task('scss', function() {
                }
              }))
 
+             // Only compile the changed files
+             .pipe(changed(pathDist))
+
              // Style Compiling Config.
-             .pipe(gulpStylelint({ // Linting
-               reporters: [
-                 {
-                   formatter: 'string',
-                   console: true
-                 }
-               ]
-             }))
+             // TODO: Fix the massive amount of issues so we can renable this
+             //.pipe(gulpStylelint({ // Linting
+             //  reporters: [
+             //    {
+             //      formatter: 'string',
+             //      console: true
+             //    }
+             //  ]
+             //}))
 
              // Initiate Source Map
              .pipe(sourcemaps.init())
@@ -72,10 +85,11 @@ gulp.task('scss', function() {
              // Write the Source Map
              .pipe(sourcemaps.write('.'))
 
+             // Plumber Stop
+             .pipe(plumber.stop())
+
              // Output
              .pipe(gulp.dest(pathDist));
-
-  browserSync.reload();
 });
 
 // The Default Watcher
